9d9be11bdb87a58920277716a5d4556e3fc99c96
mwe3 <mwe3@njit.edu>
2018-04-20 20:05:45 -0400

    Defining Constants

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,2 +1,10 @@
 import axios from "axios";
 import config from '../../config';
+
+/////////////////CONSTANTS/////////////////////
+
+
+const GET_ALL_TASKS = "GET_ALL_TASKS";
+const POST_TASK = "POST_TASK";
+const CHANGE_STATUS = "CHANGE_STATUS";
+const DELETE_TASK = "DELETE_TASK";
