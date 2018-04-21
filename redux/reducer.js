3f3ed0122d3887658d90e7a7c4b6c6591905ec1a
mwe3 <mwe3@njit.edu>
2018-04-20 20:06:53 -0400

    Setting up Actions for Dispatch

--- redux/reducer.js
+++ redux/reducer.js
@@ -8,3 +8,10 @@ const GET_ALL_TASKS = "GET_ALL_TASKS";
 const POST_TASK = "POST_TASK";
 const CHANGE_STATUS = "CHANGE_STATUS";
 const DELETE_TASK = "DELETE_TASK";
+
+/////////////////ACTIONS//////////////
+
+const getTasks = (tasks) => ({type: GET_ALL_TASKS, tasks});
+const addTask = (task) => ({type: POST_TASK, task});
+const changeStatus = (task) => ({type: CHANGE_STATUS, task});
+const taskDelete = (slug) => ({type: DELETE_TASK, slug});
