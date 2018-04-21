ba80e5eb5b93567376aa9be62c4d78cf54cae4d6
mwe3 <mwe3@njit.edu>
2018-04-20 20:08:02 -0400

    Defining Reducers

--- redux/reducer.js
+++ redux/reducer.js
@@ -15,3 +15,38 @@ const getTasks = (tasks) => ({type: GET_ALL_TASKS, tasks});
 const addTask = (task) => ({type: POST_TASK, task});
 const changeStatus = (task) => ({type: CHANGE_STATUS, task});
 const taskDelete = (slug) => ({type: DELETE_TASK, slug});
+
+/////////////////REDUCER/////////////////////
+
+//initiate your starting state
+let initial = {
+  tasks: []
+};
+
+const reducer = (state = initial, action) => {
+
+  switch (action.type) {
+    case GET_ALL_TASKS:
+      return Object.assign({}, state, {tasks: action.tasks.objects});
+    case POST_TASK:
+      let updatedTasks = [action.task].concat(state.tasks);
+      return Object.assign({}, state, {tasks: updatedTasks});
+    case CHANGE_STATUS:
+      let newArr = state.tasks.map((task) => {
+        if(task.slug === action.task.slug) task.metafields[0].value = !task.metafields[0].value;
+        return task;
+      });
+      return Object.assign({}, state, {tasks: newArr});
+    case DELETE_TASK:
+      let arr = state.tasks.filter((task) => {
+        return !(task.slug === action.slug);
+      });
+      return Object.assign({}, state, {tasks: arr});
+    default:
+      return state;
+  }
+
+};
+
+export default reducer;
+
